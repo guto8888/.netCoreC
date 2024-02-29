93dcc1c5795d269630ab56b6a9a2522cdc82b527
Carlos Silva <guto5543@gmail.com>
2024-02-29 14:08:29 -0300

    Criação página de persons

--- 1.UI/SquadManager/Controllers/PersonController.cs
+++ 1.UI/SquadManager/Controllers/PersonController.cs
@@ -5,9 +5,9 @@ namespace SquadManager.Controllers
 {
     public class PersonController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(PersonViewModel model = null)
         {
-            return View();
+            return View(model ?? new PersonViewModel());
         }
 
         public IActionResult Edit(PersonViewModel model = null)
